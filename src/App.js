df1ecd64be9ac4545802ee3b58ba347472cf6d25
kabir-webDev <kabir.webdev@gmail.com>
2021-02-14 23:02:40 +0600

    Flexiprac

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React from "react";
 import "./App.css";
 import Contacts from "./components/Contacts";
+import FlexBox from "./components/Flex/FlexBox";
 
 function App() {
   return (
-    <div className="row">
-      <div className="col-md-8 offset-md-2">
-        <Contacts />
-      </div>
+    // <div className="row">
+    //   <div className="col-md-8 offset-md-2">
+    //     <Contacts />
+    //   </div>
+    // </div>
+    <div className="">
+      <FlexBox />
     </div>
   );
 }
