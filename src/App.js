2f8580b401b506466332af3f98971d5ed977cde7
kabir-webDev <kabir.webdev@gmail.com>
2021-03-03 16:01:03 +0600

    New

--- src/App.js
+++ src/App.js
@@ -1,21 +1,8 @@
 import React from "react";
 import "./App.css";
-import Contacts from "./components/Contacts";
-import FlexBox from "./components/Flex/FlexBox";
-import RQuery from "./components/RQuery";
 
 function App() {
-  return (
-    // <div className="row">
-    //   <div className="col-md-8 offset-md-2">
-    //     <Contacts />
-    //   </div>
-    // </div>
-    <div className="App">
-      {/* <FlexBox /> */}
-      <RQuery />
-    </div>
-  );
+  return <div className="App">Boom.....</div>;
 }
 
 export default App;
