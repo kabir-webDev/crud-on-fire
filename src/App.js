1059c94b4d5f231490f49043ec8928b779b8ae0a
kabir-webDev <kabir.webdev@gmail.com>
2021-03-03 16:16:46 +0600

    Grid and Flex

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 import React from "react";
 import "./App.css";
+import MatFlex from "./components/FlexMat/MatFlex";
+import MatGrid from "./components/GridMat/MatGrid";
 
 function App() {
-  return <div className="App">Boom.....</div>;
+  return (
+    <div className="App">
+      <MatFlex />
+      <MatGrid />
+    </div>
+  );
 }
 
 export default App;
