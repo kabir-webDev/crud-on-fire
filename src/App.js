9bfec080a984e902f213feca35c41c3404ccbba9
kabir-webDev <kabir.webdev@gmail.com>
2021-03-04 00:08:08 +0600

    Card Alignment

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import React from "react";
+import "./App.css";
 import MatFlex from "./components/FlexMat/MatFlex";
+import TableData from "./components/FlexMat/TableData";
 import MatGrid from "./components/GridMat/MatGrid";
 
 function App() {
   return (
     <div>
-      <MatFlex />
+      <TableData />
+      <TableData />
+      <TableData />
+      <div className="MF">
+        <MatFlex />
+      </div>
+      <TableData />
       <MatGrid />
     </div>
   );
