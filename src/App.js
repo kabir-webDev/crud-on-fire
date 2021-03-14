eb9fc8d0f7afa2d7f518fbf89e159c7c5fd07b6e
kabir-webDev <kabir.webdev@gmail.com>
2021-03-14 23:33:55 +0600

    SS

--- src/App.js
+++ src/App.js
@@ -1,20 +1,14 @@
 import React from "react";
 import "./App.css";
-import MatFlex from "./components/FlexMat/MatFlex";
-import TableData from "./components/FlexMat/TableData";
-import MatGrid from "./components/GridMat/MatGrid";
+import Header from "./portfolio/header/Header";
 
 function App() {
   return (
     <div>
-      <TableData />
-      <TableData />
-      <TableData />
-      <div className="MF">
-        <MatFlex />
-      </div>
-      <TableData />
-      <MatGrid />
+      <Header />
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae enim
+      beatae, officia nihil sequi debitis vitae tenetur a nemo veritatis
+      repudiandae placeat explicabo ducimus quis. Sed inventore sunt obcaecati.
     </div>
   );
 }
--- src/components/FlexMat/Cardish.js
+++ src/components/FlexMat/Cardish.js
@@ -18,7 +18,7 @@ import MoreVertIcon from "@material-ui/icons/MoreVert";
 
 const useStyles = makeStyles((theme) => ({
   root: {
-    maxWidth: 300,
+    maxWidth: 350,
   },
   media: {
     height: 0,
